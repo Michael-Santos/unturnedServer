75a73fd8fb04999a8e6b844a73312ab711ea0386
BuildTools <unconfigured@null.spigotmc.org>
2021-03-14 00:12:43 -0300

    Before stops the server script will save the game now

--- stopServer.sh
+++ stopServer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if screen -ls | grep -q "unturned"; then
+  echo "Saving server"
+  screen -S unturned -p 0 -X stuff "help^M"
   echo "Stopping server..."
   screen -S unturned -X quit
 else
